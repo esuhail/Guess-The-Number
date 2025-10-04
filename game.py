f1323825d85d674d6c4997886fc69e2f5e459dc3
esuhail <esuhail731@gmail.com>
2025-10-04 16:04:25 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,24 +1,29 @@
 import random
 
 print("Guess the Number (1-100)")
-secret = random.int(1,100)
+
+secret = random.randint(1,100)
 tries = 0
 
 while True:
   guess_text = input("Enter your guess: ")
 
-if guess_text != int:
+try:
+  guess = int(guess_text)
+except ValueError:
   print("Please enter a whole number only!")
+  continue
   
   
 if guess < 1 or guess > 100:
   print("Number must be between 1 and 100!")
-  
+  continue
 
 tries += 1
 
 if guess == secret:
   print(f" Correct! {guess} was the number. It took you {tries} tries.")
+  break
 elif guess < secret:
   print("Too low!")
 else:
